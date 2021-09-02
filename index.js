5240d8134fc2a77016d6a00ad0ef1d0519745d02
sazzad-anwar <sazzadzihan@gmail.com>
2021-09-02 19:30:52 +0600

    added accesstoken and refresh token facilities

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler')
 
-//Description: Generate a token when login is successful
-exports.getToken = asyncHandler(async (data) => {
-    if (process.env.SECRET_KEY && process.env.TOKEN_EXPIRES_IN) {
-        let token = await jwt.sign(data, process.env.SECRET_KEY, {
-            expiresIn: `${process.env.TOKEN_EXPIRES_IN}`,
+//Description: Generate an access token when login is successful
+exports.getAccessToken = asyncHandler(async (data) => {
+    if (process.env.SECRET_ACCESS_TOKEN_KEY && process.env.TOKEN_EXPIRES_IN) {
+        let token = await jwt.sign(data, process.env.SECRET_ACCESS_TOKEN_KEY, {
+            expiresIn: `${process.env.ACCESS_TOKEN_EXPIRES_IN}`,
         });
         return token;
     } else {
-        throw new Error('Please set up a variable named SECRET_KEY and  TOKEN_EXPIRES_IN in your .env file');
+        throw new Error('This SecretKey is not passed as parameter and  TOKEN_EXPIRES_IN is not found in your .env file');
     }
 })
 
-//Description: add this to a protected route which needs the authentication
-//eg: router.route('/:id').all(tokenValidation, authenticated_route)
-exports.tokenValidation = asyncHandler(async (req, res, next) => {
+//Description: Generate a refresh token when login is successful
+exports.getRefreshToken = asyncHandler(async (data) => {
+    if (process.env.SECRET_ACCESS_TOKEN_KEY && process.env.TOKEN_EXPIRES_IN) {
+        let token = await jwt.sign(data, process.env.SECRET_REFRESH_TOKEN_KEY, {
+            expiresIn: `${process.env.REFRESH_TOKEN_EXPIRES_IN}`,
+        });
+        return token;
+    } else {
+        throw new Error('This SecretKey is not passed as parameter and  TOKEN_EXPIRES_IN is not found in your .env file');
+    }
+})
 
-    let token = req.headers.authorization ?? req.cookies.token
+
+//Description: add this to a protected route which needs the authentication
+exports.AccessTokenValidation = asyncHandler(async (req, res, next) => {
+
+    let token = req.headers.authorization ?? req.cookies.accessToken
 
     if (token) {
         const bearer = token.split(' ');
         const bearerToken = bearer[1];
         try {
-            var decoded = await jwt.verify(bearerToken, process.env.SECRET_KEY);
+            var decoded = await jwt.verify(bearerToken, process.env.SECRET_ACCESS_TOKEN_KEY);
             req.user = decoded;
             next()
         } catch (error) {
@@ -37,3 +49,28 @@ exports.tokenValidation = asyncHandler(async (req, res, next) => {
         throw new Error("Request Not Allowed");
     }
 });
+
+
+//Description: add this to a protected route which needs the authentication
+exports.RefreshTokenValidation = asyncHandler(async (token) => {
+
+    if (token) {
+        const bearer = token.split(' ');
+        const bearerToken = bearer[1];
+        try {
+
+            var decoded = await jwt.verify(bearerToken, process.env.SECRET_REFRESH_TOKEN_KEY);
+            return decoded;
+
+        } catch (error) {
+            console.log(error);
+            res.status(401)
+            throw new Error("Invalid token");
+        }
+    }
+    else {
+        res.status(401)
+        throw new Error("Request Not Allowed");
+    }
+});
+
