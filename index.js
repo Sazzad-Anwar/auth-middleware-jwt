5bbe2d97f79ebbe238139fc9e662e0b163cf6758
sazzad-anwar <sazzadzihan@gmail.com>
2021-09-04 03:32:12 +0600

    updated middleware error response

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ const asyncHandler = require('express-async-handler')
 
 //Description: Generate an access token when login is successful
 exports.getAccessToken = asyncHandler(async (data) => {
-    if (process.env.SECRET_ACCESS_TOKEN_KEY && process.env.TOKEN_EXPIRES_IN) {
+    if (process.env.SECRET_ACCESS_TOKEN_KEY && process.env.ACCESS_TOKEN_EXPIRES_IN) {
         let token = await jwt.sign(data, process.env.SECRET_ACCESS_TOKEN_KEY, {
             expiresIn: `${process.env.ACCESS_TOKEN_EXPIRES_IN}`,
         });
         return token;
     } else {
-        throw new Error('This SecretKey is not passed as parameter and  TOKEN_EXPIRES_IN is not found in your .env file');
+        throw new Error('SECRET_ACCESS_TOKEN_KEY and  ACCESS_TOKEN_EXPIRES_IN is not found in your .env file');
     }
 })
 
 //Description: Generate a refresh token when login is successful
 exports.getRefreshToken = asyncHandler(async (data) => {
-    if (process.env.SECRET_ACCESS_TOKEN_KEY && process.env.TOKEN_EXPIRES_IN) {
+    if (process.env.SECRET_REFRESH_TOKEN_KEY && process.env.REFRESH_TOKEN_EXPIRES_IN) {
         let token = await jwt.sign(data, process.env.SECRET_REFRESH_TOKEN_KEY, {
             expiresIn: `${process.env.REFRESH_TOKEN_EXPIRES_IN}`,
         });
         return token;
     } else {
-        throw new Error('This SecretKey is not passed as parameter and  TOKEN_EXPIRES_IN is not found in your .env file');
+        throw new Error('SECRET_REFRESH_TOKEN_KEY and  REFRESH_TOKEN_EXPIRES_IN is not found in your .env file');
     }
 })
 
@@ -39,13 +39,10 @@ exports.AccessTokenValidation = asyncHandler(async (req, res, next) => {
             req.user = decoded;
             next()
         } catch (error) {
-            console.log(error);
-            res.status(401)
             throw new Error("Request Not Allowed");
         }
     }
     else {
-        res.status(401)
         throw new Error("Request Not Allowed");
     }
 });
@@ -55,21 +52,16 @@ exports.AccessTokenValidation = asyncHandler(async (req, res, next) => {
 exports.RefreshTokenValidation = asyncHandler(async (token) => {
 
     if (token) {
-        const bearer = token.split(' ');
-        const bearerToken = bearer[1];
         try {
 
-            var decoded = await jwt.verify(bearerToken, process.env.SECRET_REFRESH_TOKEN_KEY);
+            var decoded = await jwt.verify(token, process.env.SECRET_REFRESH_TOKEN_KEY);
             return decoded;
 
         } catch (error) {
-            console.log(error);
-            res.status(401)
             throw new Error("Invalid token");
         }
     }
     else {
-        res.status(401)
         throw new Error("Request Not Allowed");
     }
 });
